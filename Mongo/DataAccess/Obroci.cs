affd4675feee972beebbc015c3b4db920ed0bf5c
nikolarank <nikolarank94@gmail.com>
2017-02-21 23:07:54 +0100

    plan

---
+++ Mongo/DataAccess/Obroci.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mongo.Documents;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+
+namespace Mongo.DataAccess
+{
+    public class Obroci
+    {
+        private static string CollectionName = "obroci";
+
+        public static void Dodaj(Obrok Item)
+        {
+            try
+            {
+                var collection = Connection<Obrok>.getCollection(CollectionName);
+                collection.Save(Item); ;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static public Obrok Procitaj(ObjectId id)
+        {
+            Obrok found = new Obrok();
+
+            try
+            {
+                var collection = Connection<Obrok>.getCollection(CollectionName);
+                return collection.FindOneByIdAs<Obrok>(id);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return default(Obrok);
+            }
+        }
+
+        static public void PromeniRedniBroj(ObjectId ObrokID, int broj)
+        {
+
+            try
+            {
+                var collection = Connection<Obrok>.getCollection(CollectionName);
+
+                Obrok k = Obroci.Procitaj(ObrokID);
+
+                k.Redni_Broj_Obroka = broj;
+
+                collection.Save(k);
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static public void DodajNamirnicu(ObjectId ObrokID, MongoDBRef nam)
+        {
+
+            try
+            {
+                var collection = Connection<Obrok>.getCollection(CollectionName);
+
+                Obrok k = Obroci.Procitaj(ObrokID);
+
+                k.Namirnice.Add(nam);
+
+                collection.Save(k);
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static public void ObrisiNamirnicu(ObjectId ObrokID, MongoDBRef nam)
+        {
+
+            try
+            {
+                var collection = Connection<Obrok>.getCollection(CollectionName);
+
+                Obrok k = Obroci.Procitaj(ObrokID);
+
+                k.Namirnice.Remove(nam);
+
+                collection.Save(k);
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
--- Mongo/DataAccess/PlanIshrane.cs
+++ Mongo/DataAccess/PlanIshrane.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Mongo.Documents;
 using MongoDB.Driver.Builders;
 using MongoDB.Bson;
@@ -12,10 +11,23 @@ using MongoDB.Driver.Linq;
 
 namespace Mongo.DataAccess
 {
-    public class PlanIshrane
+    public static class PlanIshrane
     {
         private static string CollectionName = "planovi_ishrane";
 
+        public static void Dodaj(Plan_Ishrane Item)
+        {
+            try
+            {
+                var collection = Connection<Plan_Ishrane>.getCollection(CollectionName);
+                collection.Save(Item); ;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
 
         static public Plan_Ishrane Procitaj(ObjectId id)
         {
@@ -54,5 +66,47 @@ namespace Mongo.DataAccess
                 return null;
             }
         }
+
+        static public void DodajObrok(ObjectId planId, MongoDBRef obrok)
+        {
+            try
+            {
+                var collection = Connection<Plan_Ishrane>.getCollection(CollectionName);
+
+                Plan_Ishrane k = PlanIshrane.Procitaj(planId);
+
+                k.Obroci.Add(obrok);
+
+                collection.Save(k);
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static public void ObrisiObrok(ObjectId planId, MongoDBRef obrok)
+        {
+
+            try
+            {
+                var collection = Connection<Obrok>.getCollection(CollectionName);
+
+                Obrok k = Obroci.Procitaj(planId);
+
+                k.Namirnice.Remove(obrok);
+
+                collection.Save(k);
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
     }
 }
